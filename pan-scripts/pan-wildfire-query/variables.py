f912c6463a3d9032c6c77638eea52bc353735d31
sbarber <steve@stevebarber.net>
2019-09-09 10:52:56 -0400

    Initial

--- pan-scripts/pan-wildfire-query/variables.py
+++ pan-scripts/pan-wildfire-query/variables.py
@@ -2,6 +2,5 @@
 API_KEY = '<API KEY>'
 # List of hashes
 HASH_FILE = 'test-hashes.txt'
-#HASH_FILE = 'hashes-from-Tanium-reputation-looksup.txt'
 # Name of file to log console output to
 LOG_FILE = 'logfile'
