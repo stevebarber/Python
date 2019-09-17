ae79f157b4457947d1af3bc0cf17cdaaef6a465d
sbarber <steve@stevebarber.net>
2019-09-17 14:41:51 -0400

    add ability to ignore disabled rules

---
+++ pan-scripts/pan-dynamic-objects/variables.py
@@ -0,0 +1,6 @@
+# Log filename
+LOG_FILE = 'logfile'
+# Device group to run script against
+#DEVICE_GROUP = 'DMZ_Inbound.AL'
+DEVICE_GROUP = '<device_group_name>'
+# Tag cloned and original rule with the following tag
